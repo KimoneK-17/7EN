714491b84984e370373cb3ee4d8f7d80943d871d
Martini <victormbh15@gmail.com>
2017-07-03 11:16:40 -0700

    Created the "Password Recovery" form.

---
+++ TherapyReferralSystem/TherapyReferralSystem/frmPasswordRecovery.Designer.cs
@@ -0,0 +1,330 @@
+﻿namespace TherapyReferralSystem
+{
+    partial class frmPasswordRecovery
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblSecurityQuestion = new System.Windows.Forms.Label();
+            this.txtSecurityQuestion = new System.Windows.Forms.TextBox();
+            this.btnChange = new System.Windows.Forms.Button();
+            this.btnOk = new System.Windows.Forms.Button();
+            this.lblAnswer = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnOk2 = new System.Windows.Forms.Button();
+            this.grpBxForgotAnswer = new System.Windows.Forms.GroupBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radEmail = new System.Windows.Forms.RadioButton();
+            this.radSMS = new System.Windows.Forms.RadioButton();
+            this.lblYourPassword = new System.Windows.Forms.Label();
+            this.txtYourPassword = new System.Windows.Forms.TextBox();
+            this.radSMS2 = new System.Windows.Forms.RadioButton();
+            this.radEmail2 = new System.Windows.Forms.RadioButton();
+            this.btnOK3 = new System.Windows.Forms.Button();
+            this.btnOK4 = new System.Windows.Forms.Button();
+            this.MENU = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.returnToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.grpBxForgotAnswer.SuspendLayout();
+            this.MENU.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblSecurityQuestion
+            // 
+            this.lblSecurityQuestion.AutoSize = true;
+            this.lblSecurityQuestion.BackColor = System.Drawing.Color.White;
+            this.lblSecurityQuestion.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblSecurityQuestion.Font = new System.Drawing.Font("Myriad Pro Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSecurityQuestion.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblSecurityQuestion.Location = new System.Drawing.Point(89, 50);
+            this.lblSecurityQuestion.Name = "lblSecurityQuestion";
+            this.lblSecurityQuestion.Size = new System.Drawing.Size(152, 22);
+            this.lblSecurityQuestion.TabIndex = 0;
+            this.lblSecurityQuestion.Text = "SECURITY QUESTION";
+            // 
+            // txtSecurityQuestion
+            // 
+            this.txtSecurityQuestion.Location = new System.Drawing.Point(66, 87);
+            this.txtSecurityQuestion.Name = "txtSecurityQuestion";
+            this.txtSecurityQuestion.Size = new System.Drawing.Size(197, 20);
+            this.txtSecurityQuestion.TabIndex = 1;
+            // 
+            // btnChange
+            // 
+            this.btnChange.Font = new System.Drawing.Font("Myriad Pro Light", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnChange.Location = new System.Drawing.Point(44, 129);
+            this.btnChange.Name = "btnChange";
+            this.btnChange.Size = new System.Drawing.Size(75, 28);
+            this.btnChange.TabIndex = 2;
+            this.btnChange.Text = "CHANGE";
+            this.btnChange.UseVisualStyleBackColor = true;
+            // 
+            // btnOk
+            // 
+            this.btnOk.Font = new System.Drawing.Font("Myriad Pro Light", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk.Location = new System.Drawing.Point(234, 129);
+            this.btnOk.Name = "btnOk";
+            this.btnOk.Size = new System.Drawing.Size(47, 28);
+            this.btnOk.TabIndex = 3;
+            this.btnOk.Text = "OK";
+            this.btnOk.UseVisualStyleBackColor = true;
+            // 
+            // lblAnswer
+            // 
+            this.lblAnswer.AutoSize = true;
+            this.lblAnswer.BackColor = System.Drawing.Color.White;
+            this.lblAnswer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblAnswer.Font = new System.Drawing.Font("Myriad Pro Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAnswer.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblAnswer.Location = new System.Drawing.Point(462, 52);
+            this.lblAnswer.Name = "lblAnswer";
+            this.lblAnswer.Size = new System.Drawing.Size(71, 22);
+            this.lblAnswer.TabIndex = 4;
+            this.lblAnswer.Text = "ANSWER";
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(397, 87);
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(197, 20);
+            this.txtAnswer.TabIndex = 5;
+            // 
+            // btnOk2
+            // 
+            this.btnOk2.Font = new System.Drawing.Font("Myriad Pro Light", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOk2.Location = new System.Drawing.Point(475, 122);
+            this.btnOk2.Name = "btnOk2";
+            this.btnOk2.Size = new System.Drawing.Size(47, 28);
+            this.btnOk2.TabIndex = 6;
+            this.btnOk2.Text = "OK";
+            this.btnOk2.UseVisualStyleBackColor = true;
+            // 
+            // grpBxForgotAnswer
+            // 
+            this.grpBxForgotAnswer.BackColor = System.Drawing.Color.Transparent;
+            this.grpBxForgotAnswer.Controls.Add(this.label1);
+            this.grpBxForgotAnswer.Font = new System.Drawing.Font("Myriad Pro Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpBxForgotAnswer.ForeColor = System.Drawing.Color.DarkRed;
+            this.grpBxForgotAnswer.Location = new System.Drawing.Point(66, 226);
+            this.grpBxForgotAnswer.Name = "grpBxForgotAnswer";
+            this.grpBxForgotAnswer.Size = new System.Drawing.Size(257, 90);
+            this.grpBxForgotAnswer.TabIndex = 7;
+            this.grpBxForgotAnswer.TabStop = false;
+            this.grpBxForgotAnswer.Text = "Forgot Answer?";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.White;
+            this.label1.Font = new System.Drawing.Font("Myriad Pro Light", 8.999999F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(23, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(226, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "(Skip this step if you remember the answer. \r\nIf not then select one)";
+            // 
+            // radEmail
+            // 
+            this.radEmail.AutoSize = true;
+            this.radEmail.BackColor = System.Drawing.Color.Transparent;
+            this.radEmail.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radEmail.ForeColor = System.Drawing.Color.DarkRed;
+            this.radEmail.Location = new System.Drawing.Point(113, 335);
+            this.radEmail.Name = "radEmail";
+            this.radEmail.Size = new System.Drawing.Size(109, 17);
+            this.radEmail.TabIndex = 8;
+            this.radEmail.TabStop = true;
+            this.radEmail.Text = "SEND VIA EMAIL";
+            this.radEmail.UseVisualStyleBackColor = false;
+            // 
+            // radSMS
+            // 
+            this.radSMS.AutoSize = true;
+            this.radSMS.BackColor = System.Drawing.Color.Transparent;
+            this.radSMS.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radSMS.ForeColor = System.Drawing.Color.DarkRed;
+            this.radSMS.Location = new System.Drawing.Point(113, 358);
+            this.radSMS.Name = "radSMS";
+            this.radSMS.Size = new System.Drawing.Size(100, 17);
+            this.radSMS.TabIndex = 9;
+            this.radSMS.TabStop = true;
+            this.radSMS.Text = "SEND VIA SMS";
+            this.radSMS.UseVisualStyleBackColor = false;
+            // 
+            // lblYourPassword
+            // 
+            this.lblYourPassword.AutoSize = true;
+            this.lblYourPassword.BackColor = System.Drawing.Color.White;
+            this.lblYourPassword.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.lblYourPassword.Font = new System.Drawing.Font("Myriad Pro Light", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYourPassword.ForeColor = System.Drawing.Color.DarkRed;
+            this.lblYourPassword.Location = new System.Drawing.Point(431, 226);
+            this.lblYourPassword.Name = "lblYourPassword";
+            this.lblYourPassword.Size = new System.Drawing.Size(132, 22);
+            this.lblYourPassword.TabIndex = 10;
+            this.lblYourPassword.Text = "YOUR PASSWORD";
+            // 
+            // txtYourPassword
+            // 
+            this.txtYourPassword.Location = new System.Drawing.Point(397, 267);
+            this.txtYourPassword.Name = "txtYourPassword";
+            this.txtYourPassword.Size = new System.Drawing.Size(197, 20);
+            this.txtYourPassword.TabIndex = 11;
+            // 
+            // radSMS2
+            // 
+            this.radSMS2.AutoSize = true;
+            this.radSMS2.BackColor = System.Drawing.Color.Transparent;
+            this.radSMS2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radSMS2.ForeColor = System.Drawing.Color.DarkRed;
+            this.radSMS2.Location = new System.Drawing.Point(454, 358);
+            this.radSMS2.Name = "radSMS2";
+            this.radSMS2.Size = new System.Drawing.Size(100, 17);
+            this.radSMS2.TabIndex = 13;
+            this.radSMS2.TabStop = true;
+            this.radSMS2.Text = "SEND VIA SMS";
+            this.radSMS2.UseVisualStyleBackColor = false;
+            // 
+            // radEmail2
+            // 
+            this.radEmail2.AutoSize = true;
+            this.radEmail2.BackColor = System.Drawing.Color.Transparent;
+            this.radEmail2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.radEmail2.ForeColor = System.Drawing.Color.DarkRed;
+            this.radEmail2.Location = new System.Drawing.Point(454, 335);
+            this.radEmail2.Name = "radEmail2";
+            this.radEmail2.Size = new System.Drawing.Size(109, 17);
+            this.radEmail2.TabIndex = 12;
+            this.radEmail2.TabStop = true;
+            this.radEmail2.Text = "SEND VIA EMAIL";
+            this.radEmail2.UseVisualStyleBackColor = false;
+            // 
+            // btnOK3
+            // 
+            this.btnOK3.Font = new System.Drawing.Font("Myriad Pro Light", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK3.Location = new System.Drawing.Point(252, 340);
+            this.btnOK3.Name = "btnOK3";
+            this.btnOK3.Size = new System.Drawing.Size(47, 28);
+            this.btnOK3.TabIndex = 14;
+            this.btnOK3.Text = "OK";
+            this.btnOK3.UseVisualStyleBackColor = true;
+            // 
+            // btnOK4
+            // 
+            this.btnOK4.Font = new System.Drawing.Font("Myriad Pro Light", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnOK4.Location = new System.Drawing.Point(585, 340);
+            this.btnOK4.Name = "btnOK4";
+            this.btnOK4.Size = new System.Drawing.Size(47, 28);
+            this.btnOK4.TabIndex = 15;
+            this.btnOK4.Text = "OK";
+            this.btnOK4.UseVisualStyleBackColor = true;
+            // 
+            // MENU
+            // 
+            this.MENU.BackColor = System.Drawing.Color.White;
+            this.MENU.Font = new System.Drawing.Font("Myriad Pro Light", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.MENU.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.MENU.Location = new System.Drawing.Point(0, 0);
+            this.MENU.Name = "MENU";
+            this.MENU.Size = new System.Drawing.Size(673, 26);
+            this.MENU.TabIndex = 16;
+            this.MENU.Text = "MENU";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.returnToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // returnToolStripMenuItem
+            // 
+            this.returnToolStripMenuItem.Name = "returnToolStripMenuItem";
+            this.returnToolStripMenuItem.Size = new System.Drawing.Size(152, 22);
+            this.returnToolStripMenuItem.Text = "Return";
+            // 
+            // frmPasswordRecovery
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(673, 407);
+            this.Controls.Add(this.btnOK4);
+            this.Controls.Add(this.btnOK3);
+            this.Controls.Add(this.radSMS2);
+            this.Controls.Add(this.radEmail2);
+            this.Controls.Add(this.txtYourPassword);
+            this.Controls.Add(this.lblYourPassword);
+            this.Controls.Add(this.radSMS);
+            this.Controls.Add(this.radEmail);
+            this.Controls.Add(this.grpBxForgotAnswer);
+            this.Controls.Add(this.btnOk2);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblAnswer);
+            this.Controls.Add(this.btnOk);
+            this.Controls.Add(this.btnChange);
+            this.Controls.Add(this.txtSecurityQuestion);
+            this.Controls.Add(this.lblSecurityQuestion);
+            this.Controls.Add(this.MENU);
+            this.MainMenuStrip = this.MENU;
+            this.Name = "frmPasswordRecovery";
+            this.Text = "Password Recovery";
+            this.grpBxForgotAnswer.ResumeLayout(false);
+            this.grpBxForgotAnswer.PerformLayout();
+            this.MENU.ResumeLayout(false);
+            this.MENU.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblSecurityQuestion;
+        private System.Windows.Forms.TextBox txtSecurityQuestion;
+        private System.Windows.Forms.Button btnChange;
+        private System.Windows.Forms.Button btnOk;
+        private System.Windows.Forms.Label lblAnswer;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnOk2;
+        private System.Windows.Forms.GroupBox grpBxForgotAnswer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radEmail;
+        private System.Windows.Forms.RadioButton radSMS;
+        private System.Windows.Forms.Label lblYourPassword;
+        private System.Windows.Forms.TextBox txtYourPassword;
+        private System.Windows.Forms.RadioButton radSMS2;
+        private System.Windows.Forms.RadioButton radEmail2;
+        private System.Windows.Forms.Button btnOK3;
+        private System.Windows.Forms.Button btnOK4;
+        private System.Windows.Forms.MenuStrip MENU;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem returnToolStripMenuItem;
+    }
+}
