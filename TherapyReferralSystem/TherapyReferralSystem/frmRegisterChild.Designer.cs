3cb0a91ce2efaaf15ca24d0ab2e1969f25e665cb
Kimone <kimone001@gmail.com>
2017-07-16 19:10:52 +0200

    Child Screen
    Design for screen to capture child information

---
+++ TherapyReferralSystem/TherapyReferralSystem/frmRegisterChild.Designer.cs
@@ -0,0 +1,343 @@
+﻿namespace TherapyReferralSystem
+{
+    partial class frmRegisterChild
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnSubmit = new System.Windows.Forms.Button();
+            this.rtbxSpecCon = new System.Windows.Forms.RichTextBox();
+            this.lblSpecCon = new System.Windows.Forms.Label();
+            this.dtpDOF = new System.Windows.Forms.DateTimePicker();
+            this.lblDOF = new System.Windows.Forms.Label();
+            this.txtID = new System.Windows.Forms.TextBox();
+            this.lblID = new System.Windows.Forms.Label();
+            this.cmbGender = new System.Windows.Forms.ComboBox();
+            this.lblGender = new System.Windows.Forms.Label();
+            this.cmbBSF = new System.Windows.Forms.ComboBox();
+            this.lblBSF = new System.Windows.Forms.Label();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.lblLName = new System.Windows.Forms.Label();
+            this.txtMName = new System.Windows.Forms.TextBox();
+            this.txtFName = new System.Windows.Forms.TextBox();
+            this.lblMName = new System.Windows.Forms.Label();
+            this.lblFName = new System.Windows.Forms.Label();
+            this.cmbHouse = new System.Windows.Forms.ComboBox();
+            this.lblHouse = new System.Windows.Forms.Label();
+            this.cmbCluster = new System.Windows.Forms.ComboBox();
+            this.lblCluster = new System.Windows.Forms.Label();
+            this.cmbStatus = new System.Windows.Forms.ComboBox();
+            this.lblStatus = new System.Windows.Forms.Label();
+            this.lblNumber = new System.Windows.Forms.Label();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(554, 430);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.btnClear.Size = new System.Drawing.Size(75, 23);
+            this.btnClear.TabIndex = 51;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            // 
+            // btnSubmit
+            // 
+            this.btnSubmit.Location = new System.Drawing.Point(554, 459);
+            this.btnSubmit.Name = "btnSubmit";
+            this.btnSubmit.Size = new System.Drawing.Size(75, 23);
+            this.btnSubmit.TabIndex = 50;
+            this.btnSubmit.Text = "Submit";
+            this.btnSubmit.UseVisualStyleBackColor = true;
+            // 
+            // rtbxSpecCon
+            // 
+            this.rtbxSpecCon.Location = new System.Drawing.Point(189, 435);
+            this.rtbxSpecCon.Name = "rtbxSpecCon";
+            this.rtbxSpecCon.Size = new System.Drawing.Size(334, 96);
+            this.rtbxSpecCon.TabIndex = 49;
+            this.rtbxSpecCon.Text = "";
+            // 
+            // lblSpecCon
+            // 
+            this.lblSpecCon.AutoSize = true;
+            this.lblSpecCon.Location = new System.Drawing.Point(12, 436);
+            this.lblSpecCon.Name = "lblSpecCon";
+            this.lblSpecCon.Size = new System.Drawing.Size(152, 17);
+            this.lblSpecCon.TabIndex = 48;
+            this.lblSpecCon.Text = "Special Considerations";
+            // 
+            // dtpDOF
+            // 
+            this.dtpDOF.Location = new System.Drawing.Point(189, 399);
+            this.dtpDOF.Name = "dtpDOF";
+            this.dtpDOF.Size = new System.Drawing.Size(200, 22);
+            this.dtpDOF.TabIndex = 47;
+            // 
+            // lblDOF
+            // 
+            this.lblDOF.AutoSize = true;
+            this.lblDOF.Location = new System.Drawing.Point(12, 399);
+            this.lblDOF.Name = "lblDOF";
+            this.lblDOF.Size = new System.Drawing.Size(122, 17);
+            this.lblDOF.TabIndex = 46;
+            this.lblDOF.Text = "Date of Admission";
+            // 
+            // txtID
+            // 
+            this.txtID.Location = new System.Drawing.Point(189, 363);
+            this.txtID.Name = "txtID";
+            this.txtID.Size = new System.Drawing.Size(202, 22);
+            this.txtID.TabIndex = 45;
+            // 
+            // lblID
+            // 
+            this.lblID.AutoSize = true;
+            this.lblID.Location = new System.Drawing.Point(12, 362);
+            this.lblID.Name = "lblID";
+            this.lblID.Size = new System.Drawing.Size(75, 17);
+            this.lblID.TabIndex = 44;
+            this.lblID.Text = "ID Number";
+            // 
+            // cmbGender
+            // 
+            this.cmbGender.FormattingEnabled = true;
+            this.cmbGender.Location = new System.Drawing.Point(189, 325);
+            this.cmbGender.Name = "cmbGender";
+            this.cmbGender.Size = new System.Drawing.Size(202, 24);
+            this.cmbGender.TabIndex = 43;
+            // 
+            // lblGender
+            // 
+            this.lblGender.AutoSize = true;
+            this.lblGender.Location = new System.Drawing.Point(12, 325);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(56, 17);
+            this.lblGender.TabIndex = 42;
+            this.lblGender.Text = "Gender";
+            // 
+            // cmbBSF
+            // 
+            this.cmbBSF.FormattingEnabled = true;
+            this.cmbBSF.Location = new System.Drawing.Point(189, 179);
+            this.cmbBSF.Name = "cmbBSF";
+            this.cmbBSF.Size = new System.Drawing.Size(202, 24);
+            this.cmbBSF.TabIndex = 41;
+            // 
+            // lblBSF
+            // 
+            this.lblBSF.AutoSize = true;
+            this.lblBSF.Location = new System.Drawing.Point(12, 177);
+            this.lblBSF.Name = "lblBSF";
+            this.lblBSF.Size = new System.Drawing.Size(42, 17);
+            this.lblBSF.TabIndex = 40;
+            this.lblBSF.Text = "B/S/F";
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(189, 289);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(202, 22);
+            this.txtLName.TabIndex = 39;
+            // 
+            // lblLName
+            // 
+            this.lblLName.AutoSize = true;
+            this.lblLName.Location = new System.Drawing.Point(12, 288);
+            this.lblLName.Name = "lblLName";
+            this.lblLName.Size = new System.Drawing.Size(70, 17);
+            this.lblLName.TabIndex = 38;
+            this.lblLName.Text = "Lastname";
+            // 
+            // txtMName
+            // 
+            this.txtMName.Location = new System.Drawing.Point(189, 253);
+            this.txtMName.Name = "txtMName";
+            this.txtMName.Size = new System.Drawing.Size(202, 22);
+            this.txtMName.TabIndex = 37;
+            // 
+            // txtFName
+            // 
+            this.txtFName.Location = new System.Drawing.Point(189, 217);
+            this.txtFName.Name = "txtFName";
+            this.txtFName.Size = new System.Drawing.Size(202, 22);
+            this.txtFName.TabIndex = 36;
+            // 
+            // lblMName
+            // 
+            this.lblMName.AutoSize = true;
+            this.lblMName.Location = new System.Drawing.Point(12, 251);
+            this.lblMName.Name = "lblMName";
+            this.lblMName.Size = new System.Drawing.Size(90, 17);
+            this.lblMName.TabIndex = 35;
+            this.lblMName.Text = "Middle Name";
+            // 
+            // lblFName
+            // 
+            this.lblFName.AutoSize = true;
+            this.lblFName.Location = new System.Drawing.Point(12, 214);
+            this.lblFName.Name = "lblFName";
+            this.lblFName.Size = new System.Drawing.Size(70, 17);
+            this.lblFName.TabIndex = 34;
+            this.lblFName.Text = "Firstname";
+            // 
+            // cmbHouse
+            // 
+            this.cmbHouse.FormattingEnabled = true;
+            this.cmbHouse.Location = new System.Drawing.Point(189, 141);
+            this.cmbHouse.Name = "cmbHouse";
+            this.cmbHouse.Size = new System.Drawing.Size(202, 24);
+            this.cmbHouse.TabIndex = 33;
+            // 
+            // lblHouse
+            // 
+            this.lblHouse.AutoSize = true;
+            this.lblHouse.Location = new System.Drawing.Point(12, 140);
+            this.lblHouse.Name = "lblHouse";
+            this.lblHouse.Size = new System.Drawing.Size(49, 17);
+            this.lblHouse.TabIndex = 32;
+            this.lblHouse.Text = "House";
+            // 
+            // cmbCluster
+            // 
+            this.cmbCluster.FormattingEnabled = true;
+            this.cmbCluster.Location = new System.Drawing.Point(189, 103);
+            this.cmbCluster.Name = "cmbCluster";
+            this.cmbCluster.Size = new System.Drawing.Size(202, 24);
+            this.cmbCluster.TabIndex = 31;
+            // 
+            // lblCluster
+            // 
+            this.lblCluster.AutoSize = true;
+            this.lblCluster.Location = new System.Drawing.Point(12, 103);
+            this.lblCluster.Name = "lblCluster";
+            this.lblCluster.Size = new System.Drawing.Size(52, 17);
+            this.lblCluster.TabIndex = 30;
+            this.lblCluster.Text = "Cluster";
+            // 
+            // cmbStatus
+            // 
+            this.cmbStatus.FormattingEnabled = true;
+            this.cmbStatus.Location = new System.Drawing.Point(189, 65);
+            this.cmbStatus.Name = "cmbStatus";
+            this.cmbStatus.Size = new System.Drawing.Size(200, 24);
+            this.cmbStatus.TabIndex = 29;
+            // 
+            // lblStatus
+            // 
+            this.lblStatus.AutoSize = true;
+            this.lblStatus.Location = new System.Drawing.Point(12, 66);
+            this.lblStatus.Name = "lblStatus";
+            this.lblStatus.Size = new System.Drawing.Size(48, 17);
+            this.lblStatus.TabIndex = 28;
+            this.lblStatus.Text = "Status";
+            // 
+            // lblNumber
+            // 
+            this.lblNumber.AutoSize = true;
+            this.lblNumber.Location = new System.Drawing.Point(12, 29);
+            this.lblNumber.Name = "lblNumber";
+            this.lblNumber.Size = new System.Drawing.Size(58, 17);
+            this.lblNumber.TabIndex = 27;
+            this.lblNumber.Text = "Number";
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(189, 29);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(200, 22);
+            this.txtNumber.TabIndex = 26;
+            // 
+            // frmRegisterChild
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 568);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubmit);
+            this.Controls.Add(this.rtbxSpecCon);
+            this.Controls.Add(this.lblSpecCon);
+            this.Controls.Add(this.dtpDOF);
+            this.Controls.Add(this.lblDOF);
+            this.Controls.Add(this.txtID);
+            this.Controls.Add(this.lblID);
+            this.Controls.Add(this.cmbGender);
+            this.Controls.Add(this.lblGender);
+            this.Controls.Add(this.cmbBSF);
+            this.Controls.Add(this.lblBSF);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.lblLName);
+            this.Controls.Add(this.txtMName);
+            this.Controls.Add(this.txtFName);
+            this.Controls.Add(this.lblMName);
+            this.Controls.Add(this.lblFName);
+            this.Controls.Add(this.cmbHouse);
+            this.Controls.Add(this.lblHouse);
+            this.Controls.Add(this.cmbCluster);
+            this.Controls.Add(this.lblCluster);
+            this.Controls.Add(this.cmbStatus);
+            this.Controls.Add(this.lblStatus);
+            this.Controls.Add(this.lblNumber);
+            this.Controls.Add(this.txtNumber);
+            this.Name = "frmRegisterChild";
+            this.Text = "Register Child";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnSubmit;
+        private System.Windows.Forms.RichTextBox rtbxSpecCon;
+        private System.Windows.Forms.Label lblSpecCon;
+        private System.Windows.Forms.DateTimePicker dtpDOF;
+        private System.Windows.Forms.Label lblDOF;
+        private System.Windows.Forms.TextBox txtID;
+        private System.Windows.Forms.Label lblID;
+        private System.Windows.Forms.ComboBox cmbGender;
+        private System.Windows.Forms.Label lblGender;
+        private System.Windows.Forms.ComboBox cmbBSF;
+        private System.Windows.Forms.Label lblBSF;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.Label lblLName;
+        private System.Windows.Forms.TextBox txtMName;
+        private System.Windows.Forms.TextBox txtFName;
+        private System.Windows.Forms.Label lblMName;
+        private System.Windows.Forms.Label lblFName;
+        private System.Windows.Forms.ComboBox cmbHouse;
+        private System.Windows.Forms.Label lblHouse;
+        private System.Windows.Forms.ComboBox cmbCluster;
+        private System.Windows.Forms.Label lblCluster;
+        private System.Windows.Forms.ComboBox cmbStatus;
+        private System.Windows.Forms.Label lblStatus;
+        private System.Windows.Forms.Label lblNumber;
+        private System.Windows.Forms.TextBox txtNumber;
+    }
+}
