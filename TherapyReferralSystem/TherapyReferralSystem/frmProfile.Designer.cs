42397dc738cec40c6ccfe2d9eeaa4227afe7ed64
Kimone <kimone001@gmail.com>
2017-08-16 08:40:51 +0200

    changed frmProfile name to User Profile

--- TherapyReferralSystem/TherapyReferralSystem/frmProfile.Designer.cs
+++ TherapyReferralSystem/TherapyReferralSystem/frmProfile.Designer.cs
@@ -56,155 +56,172 @@
             // 
             // menuStrip1
             // 
+            this.menuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.fileToolStripMenuItem,
             this.helpToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(450, 24);
+            this.menuStrip1.Padding = new System.Windows.Forms.Padding(8, 2, 0, 2);
+            this.menuStrip1.Size = new System.Drawing.Size(600, 28);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
             // fileToolStripMenuItem
             // 
             this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
-            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(44, 24);
             this.fileToolStripMenuItem.Text = "File";
             // 
             // helpToolStripMenuItem
             // 
             this.helpToolStripMenuItem.Name = "helpToolStripMenuItem";
-            this.helpToolStripMenuItem.Size = new System.Drawing.Size(44, 20);
+            this.helpToolStripMenuItem.Size = new System.Drawing.Size(53, 24);
             this.helpToolStripMenuItem.Text = "Help";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(187, 33);
+            this.label1.Location = new System.Drawing.Point(249, 41);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(36, 13);
+            this.label1.Size = new System.Drawing.Size(48, 17);
             this.label1.TabIndex = 1;
             this.label1.Text = "Profile";
             // 
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.pictureBox1.Location = new System.Drawing.Point(278, 70);
+            this.pictureBox1.Location = new System.Drawing.Point(371, 86);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(133, 173);
+            this.pictureBox1.Size = new System.Drawing.Size(176, 212);
             this.pictureBox1.TabIndex = 2;
             this.pictureBox1.TabStop = false;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(25, 75);
+            this.label2.Location = new System.Drawing.Point(33, 92);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(38, 13);
+            this.label2.Size = new System.Drawing.Size(49, 17);
             this.label2.TabIndex = 3;
             this.label2.Text = "Name ";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(25, 100);
+            this.label3.Location = new System.Drawing.Point(33, 123);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(52, 13);
+            this.label3.Size = new System.Drawing.Size(69, 17);
             this.label3.TabIndex = 4;
             this.label3.Text = "Surname ";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(25, 127);
+            this.label4.Location = new System.Drawing.Point(33, 156);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(76, 13);
+            this.label4.Size = new System.Drawing.Size(101, 17);
             this.label4.TabIndex = 5;
             this.label4.Text = "Phone number";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(25, 184);
+            this.label5.Location = new System.Drawing.Point(33, 226);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(44, 13);
+            this.label5.Size = new System.Drawing.Size(58, 17);
             this.label5.TabIndex = 6;
             this.label5.Text = "Position";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(25, 212);
+            this.label6.Location = new System.Drawing.Point(33, 261);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(18, 13);
+            this.label6.Size = new System.Drawing.Size(21, 17);
             this.label6.TabIndex = 7;
             this.label6.Text = "ID";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(25, 243);
+            this.label7.Location = new System.Drawing.Point(33, 299);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(68, 13);
+            this.label7.Size = new System.Drawing.Size(86, 17);
             this.label7.TabIndex = 8;
             this.label7.Text = "Work ID ???";
             // 
             // txtName
             // 
-            this.txtName.Location = new System.Drawing.Point(131, 72);
+            this.txtName.Location = new System.Drawing.Point(175, 89);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtName.Name = "txtName";
-            this.txtName.Size = new System.Drawing.Size(128, 20);
+            this.txtName.Size = new System.Drawing.Size(169, 22);
             this.txtName.TabIndex = 9;
             this.txtName.Text = "able to edit";
             this.txtName.TextChanged += new System.EventHandler(this.txtName_TextChanged);
             // 
             // txtSurname
             // 
-            this.txtSurname.Location = new System.Drawing.Point(131, 97);
+            this.txtSurname.Location = new System.Drawing.Point(175, 119);
+            this.txtSurname.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtSurname.Name = "txtSurname";
-            this.txtSurname.Size = new System.Drawing.Size(128, 20);
+            this.txtSurname.Size = new System.Drawing.Size(169, 22);
             this.txtSurname.TabIndex = 10;
             this.txtSurname.Text = "able to edit";
             this.txtSurname.TextChanged += new System.EventHandler(this.txtSurname_TextChanged);
             // 
             // txtPhoneNumber
             // 
-            this.txtPhoneNumber.Location = new System.Drawing.Point(131, 124);
+            this.txtPhoneNumber.Location = new System.Drawing.Point(175, 153);
+            this.txtPhoneNumber.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPhoneNumber.Name = "txtPhoneNumber";
-            this.txtPhoneNumber.Size = new System.Drawing.Size(128, 20);
+            this.txtPhoneNumber.Size = new System.Drawing.Size(169, 22);
             this.txtPhoneNumber.TabIndex = 11;
             this.txtPhoneNumber.Text = "able to edit";
             this.txtPhoneNumber.TextChanged += new System.EventHandler(this.txtPhoneNumber_TextChanged);
             // 
             // txtPosition
             // 
-            this.txtPosition.Location = new System.Drawing.Point(131, 181);
+            this.txtPosition.Location = new System.Drawing.Point(175, 223);
+            this.txtPosition.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPosition.Name = "txtPosition";
-            this.txtPosition.Size = new System.Drawing.Size(128, 20);
+            this.txtPosition.Size = new System.Drawing.Size(169, 22);
             this.txtPosition.TabIndex = 12;
             this.txtPosition.Text = "shouldnt be able to edit";
             // 
             // txtID
             // 
-            this.txtID.Location = new System.Drawing.Point(131, 209);
+            this.txtID.Location = new System.Drawing.Point(175, 257);
+            this.txtID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtID.Name = "txtID";
-            this.txtID.Size = new System.Drawing.Size(128, 20);
+            this.txtID.Size = new System.Drawing.Size(169, 22);
             this.txtID.TabIndex = 13;
             this.txtID.Text = "shouldnt be able to edit";
             // 
             // txtWorkID
             // 
-            this.txtWorkID.Location = new System.Drawing.Point(131, 240);
+            this.txtWorkID.Location = new System.Drawing.Point(175, 295);
+            this.txtWorkID.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtWorkID.Name = "txtWorkID";
-            this.txtWorkID.Size = new System.Drawing.Size(128, 20);
+            this.txtWorkID.Size = new System.Drawing.Size(169, 22);
             this.txtWorkID.TabIndex = 14;
             this.txtWorkID.Text = "shouldnt be able to edit";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(28, 285);
+            this.button1.Location = new System.Drawing.Point(37, 351);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(117, 23);
+            this.button1.Size = new System.Drawing.Size(156, 28);
             this.button1.TabIndex = 15;
             this.button1.Text = "Update information";
             this.button1.UseVisualStyleBackColor = true;
@@ -212,17 +229,19 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(323, 54);
+            this.label8.Location = new System.Drawing.Point(431, 66);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(41, 13);
+            this.label8.Size = new System.Drawing.Size(52, 17);
             this.label8.TabIndex = 16;
             this.label8.Text = "pic box";
             // 
             // txtPassword
             // 
-            this.txtPassword.Location = new System.Drawing.Point(131, 152);
+            this.txtPassword.Location = new System.Drawing.Point(175, 187);
+            this.txtPassword.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtPassword.Name = "txtPassword";
-            this.txtPassword.Size = new System.Drawing.Size(128, 20);
+            this.txtPassword.Size = new System.Drawing.Size(169, 22);
             this.txtPassword.TabIndex = 18;
             this.txtPassword.Text = "able to edit";
             this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
@@ -230,26 +249,28 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(25, 155);
+            this.label9.Location = new System.Drawing.Point(33, 191);
+            this.label9.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label9.Name = "label9";
-            this.label9.Size = new System.Drawing.Size(53, 13);
+            this.label9.Size = new System.Drawing.Size(69, 17);
             this.label9.TabIndex = 17;
             this.label9.Text = "Password";
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(299, 285);
+            this.button2.Location = new System.Drawing.Point(399, 351);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(112, 23);
+            this.button2.Size = new System.Drawing.Size(149, 28);
             this.button2.TabIndex = 19;
             this.button2.Text = "Change picture ???";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // frmProfile
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(450, 320);
+            this.ClientSize = new System.Drawing.Size(600, 394);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.txtPassword);
             this.Controls.Add(this.label9);
@@ -271,8 +292,9 @@
             this.Controls.Add(this.label1);
             this.Controls.Add(this.menuStrip1);
             this.MainMenuStrip = this.menuStrip1;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "frmProfile";
-            this.Text = "frmProfile";
+            this.Text = "User Profile";
             this.menuStrip1.ResumeLayout(false);
             this.menuStrip1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
