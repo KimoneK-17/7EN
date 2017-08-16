0d0ff9dbcea3c81d260fba910c79306dc4603893
Kimone <kimone001@gmail.com>
2017-08-16 09:54:07 +0200

    Added DB Connect Class

---
+++ TherapyReferralSystem/TherapyReferralSystem/DBConnect.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TestingForms
+{
+
+        class DBConnect
+        {
+            public SqlConnection sqlConn;
+            public SqlCommand sqlCmd;
+            public SqlDataReader sqlDR;
+            public SqlDataAdapter sqlDA;
+            public void OpenConnection()
+            {
+                try
+                {
+                    sqlConn = new SqlConnection();
+                    sqlConn.ConnectionString = ("Server=tcp:livserver.database.windows.net,1433;Initial Catalog=LIVDatabase;Persist Security Info=False;User ID=LIV;Password=abcd1234$;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;");
+                    sqlConn.Open();
+                MessageBox.Show("Success");
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("ERROR - Connection not established.\n" + ex.Message);
+                }
+            }
+
+            public void GiveCommand(string sSql)
+            {
+                OpenConnection();
+                sqlCmd = new SqlCommand();
+                sqlCmd.Connection = sqlConn;
+                sqlCmd.CommandText = sSql;
+            }
+        }
+    
+
+}
+
