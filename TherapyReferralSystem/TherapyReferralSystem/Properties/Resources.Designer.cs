1707ee7f892e75d47719bc2001eafce97d0a8ca1
Kimone <kimone001@gmail.com>
2017-10-27 09:06:46 +0200

    therapy ref background

--- TherapyReferralSystem/TherapyReferralSystem/Properties/Resources.Designer.cs
+++ TherapyReferralSystem/TherapyReferralSystem/Properties/Resources.Designer.cs
@@ -139,5 +139,15 @@ namespace TherapyReferralSystem.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap therapyback {
+            get {
+                object obj = ResourceManager.GetObject("therapyback", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
